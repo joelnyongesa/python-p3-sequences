d8d4b51ff7d5a63355074eeaa6c26c1b634d037e
joelnyongesa <joelnyongesa148@gmail.com>
2023-09-13 12:52:58 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    a = 0  
+    b = 1  
+
+    for i in range(0,length):
+        if length == 0:
+            return my_list
+        else:
+            my_list.append(a)
+            c = a + b
+            a = b
+            b = c
+    return print(my_list)
